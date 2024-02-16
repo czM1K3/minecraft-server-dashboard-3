eb50424d298b3df35b9f66dad240bd66907abab5
czM1K3 <misamadera1@gmail.com>
2024-02-16 17:58:43 +0100

    Add docker

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const jiti = createJiti(new URL(import.meta.url).pathname);
 jiti("./src/env");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: "standalone",
+};
 
 export default nextConfig;
